29068856d25e37a8eebf27f3b4f16ffdda768481
Benedicto Merales Jr <troymerales@gmail.com>
2026-04-23 14:12:54 +0800

    EDA + Naive Bayes Model

--- database/final_table.sql
+++ database/final_table.sql
@@ -1,45 +1,57 @@
+DROP TABLE IF EXISTS cleaned_data;
+
 CREATE TABLE cleaned_data AS
-SELECT
-    (SELECT label FROM pollutant_levels p 
-     WHERE p.pollutant = 'pm25' AND phq2_raw.pm25 <= p.max_value
-     ORDER BY p.max_value
-     LIMIT 1) AS pm25,
 
-    (SELECT label FROM pollutant_levels p 
-     WHERE p.pollutant = 'pm10' AND phq2_raw.pm10 <= p.max_value
-     ORDER BY p.max_value
-     LIMIT 1) AS pm10,
+WITH agg AS (
+    SELECT
+        (SELECT label FROM pollutant_levels p 
+         WHERE p.pollutant = 'pm25' AND phq2_raw.pm25 <= p.max_value
+         ORDER BY p.max_value
+         LIMIT 1) AS pm25,
 
-    (SELECT label FROM pollutant_levels p 
-     WHERE p.pollutant = 'no2' AND phq2_raw.no2 <= p.max_value
-     ORDER BY p.max_value
-     LIMIT 1) AS no2,
+        (SELECT label FROM pollutant_levels p 
+         WHERE p.pollutant = 'pm10' AND phq2_raw.pm10 <= p.max_value
+         ORDER BY p.max_value
+         LIMIT 1) AS pm10,
 
-    (SELECT label FROM pollutant_levels p 
-     WHERE p.pollutant = 'so2' AND phq2_raw.so2 <= p.max_value
-     ORDER BY p.max_value
-     LIMIT 1) AS so2,
+        (SELECT label FROM pollutant_levels p 
+         WHERE p.pollutant = 'no2' AND phq2_raw.no2 <= p.max_value
+         ORDER BY p.max_value
+         LIMIT 1) AS no2,
 
-    (SELECT label FROM pollutant_levels p 
-     WHERE p.pollutant = 'co' AND phq2_raw.co <= p.max_value
-     ORDER BY p.max_value
-     LIMIT 1) AS co,
+        (SELECT label FROM pollutant_levels p 
+         WHERE p.pollutant = 'so2' AND phq2_raw.so2 <= p.max_value
+         ORDER BY p.max_value
+         LIMIT 1) AS so2,
 
-    (SELECT label FROM pollutant_levels p 
-     WHERE p.pollutant = 'o3' AND phq2_raw.o3 <= p.max_value
-     ORDER BY p.max_value
-     LIMIT 1) AS o3,
+        (SELECT label FROM pollutant_levels p 
+         WHERE p.pollutant = 'co' AND phq2_raw.co <= p.max_value
+         ORDER BY p.max_value
+         LIMIT 1) AS co,
 
-    CASE 
-		WHEN case_count > 0 THEN 'True'
-		ELSE 'False'
-	END AS new_case,
-	
-	CASE 
-		WHEN death_count > 0 THEN 'True'
-		ELSE 'False'
-	END AS new_death
+        (SELECT label FROM pollutant_levels p 
+         WHERE p.pollutant = 'o3' AND phq2_raw.o3 <= p.max_value
+         ORDER BY p.max_value
+         LIMIT 1) AS o3,
 
-FROM phq2_raw;
+        CASE 
+            WHEN case_count > 0 THEN 'True'
+            ELSE 'False'
+        END AS new_case,
+        
+        CASE 
+            WHEN death_count > 0 THEN 'True'
+            ELSE 'False'
+        END AS new_death
 
-SELECT * FROM cleaned_data;
+    FROM phq2_raw
+)
+
+SELECT *
+FROM agg
+WHERE pm25 IS NOT NULL
+  AND pm10 IS NOT NULL
+  AND no2 IS NOT NULL
+  AND so2 IS NOT NULL
+  AND co IS NOT NULL
+  AND o3 IS NOT NULL;
